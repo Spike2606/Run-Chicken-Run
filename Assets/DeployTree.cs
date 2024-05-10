48967ba2e5bde80a63f17a01de45aef10c962c48
Jan Tomczyk <jantomczyk2606@gmail.com>
2024-05-11 00:32:44 +0200

    Update

---
+++ Assets/DeployTree.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployTree : MonoBehaviour
+{
+    public GameObject treePrefab;
+    public float respawnTime = 1.0f;
+    private float screenHalfWidth;
+    private float spawnYPosition;
+
+    void Start()
+    {
+        // Calculate half of the screen width in world units
+        screenHalfWidth = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+        
+        // Calculate the Y position for spawning trees (e.g., the bottom of the screen)
+        spawnYPosition = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y;
+
+        // Start the TreeWave coroutine
+        StartCoroutine(TreeWave());
+    }
+
+    private void SpawnEnemy()
+    {
+        // Calculate a random X position within the screen bounds
+        float spawnXPosition = Random.Range(-screenHalfWidth, screenHalfWidth);
+
+        // Instantiate the tree at the calculated position
+        Instantiate(treePrefab, new Vector2(spawnXPosition, spawnYPosition), Quaternion.identity);
+    }
+
+    private IEnumerator TreeWave()
+    {
+        while (true)
+        {
+            // Wait for the specified respawn time
+            yield return new WaitForSeconds(respawnTime);
+
+            // Spawn a tree
+            SpawnEnemy();
+        }
+    }
+}
--- Assets/TreeMovement.cs
+++ Assets/TreeMovement.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class TreeMovement : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 8.0f;
     private Rigidbody2D rb;
+    private Vector2 screenBounds;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, speed);
+        screenBounds=Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < -screenBounds.y)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
